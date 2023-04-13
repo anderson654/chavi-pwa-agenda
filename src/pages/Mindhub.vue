9ea209370fe61e188bcf6f9eab1c6321bb3f163e
ViniciusPRosa <vinipiazzetta@gmail.com>
2023-04-13 08:58:52 -0300

    fix: :bug: Removido conflito de rotas
    o Index redirecionava para o login que é exclusivo da hotmilk, agora faz isso apenas se for hotmilk

--- src/pages/Mindhub.vue
+++ src/pages/Mindhub.vue
@@ -228,7 +228,7 @@ export default {
         url: "Imoveis/retornarImoveisAgendamento",
         method: "get",
         params: {
-          entidadeId: "618ec1c37ec86fa253e9d3ab",
+          entidadeId: "642da32cb5e1fd78ec65767e",
         },
       });
       if (response.status == 200) {
